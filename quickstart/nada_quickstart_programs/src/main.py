8253da02224e2c5adef61d6a3e6be3306b083869
Abhishek Sharma <abhisheksharma620703@gmail.com>
2024-07-16 13:44:01 +0000

    Audit Score Calculation Program

--- quickstart/nada_quickstart_programs/src/main.py
+++ quickstart/nada_quickstart_programs/src/main.py
@@ -1,12 +1,28 @@
 from nada_dsl import *
 
 def nada_main():
-    party1 = Party(name="Party1")
-    party2 = Party(name="Party2")
-    party3 = Party(name="Party3")
-    a = SecretInteger(Input(name="A", party=party1))
-    b = SecretInteger(Input(name="B", party=party2))
+    business = Party(name="Business")
+    auditor = Party(name="Auditor")
+    
+    financial_health = SecretInteger(Input(name="financial_health", party=business))
+    compliance = SecretInteger(Input(name="compliance", party=business))
+    operational_efficiency = SecretInteger(Input(name="operational_efficiency", party=business))
+    risk_management = SecretInteger(Input(name="risk_management", party=business))
 
-    result = a + b
+    weight_financial_health = SecretInteger(Input(name="weight_financial_health", party=auditor))
+    weight_compliance = SecretInteger(Input(name="weight_compliance", party=auditor))
+    weight_operational_efficiency = SecretInteger(Input(name="weight_operational_efficiency", party=auditor))
+    weight_risk_management = SecretInteger(Input(name="weight_risk_management", party=auditor))
 
-    return [Output(result, "my_output", party3)]
+    total_weighted_score = (financial_health * weight_financial_health +
+                            compliance * weight_compliance +
+                            operational_efficiency * weight_operational_efficiency +
+                            risk_management * weight_risk_management)
+
+    total_weight = weight_financial_health + weight_compliance + weight_operational_efficiency + weight_risk_management
+
+    weighted_average_score = total_weighted_score / total_weight
+
+    return [Output(weighted_average_score, "audit_score", business)]
+
+
